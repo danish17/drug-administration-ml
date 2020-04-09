7268f42c961938efe6371b58ba2f2368da1eefa9
Danish Shakeel <danishshakeel54@gmail.com>
2020-04-10 01:07:15 +0530

    Complete redesign, added new features

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 from flask import Flask,render_template,request
-from src import predict_using_level
+from src import predict
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/using_levels', methods=['POST'])
 def using_levels():
     age = request.form.get('age')
     sex = request.form.get('sex')
     level = request.form.get('level')
+    comorb = request.form.get('comorb')
 
     sex = 0 if sex.casefold() == "female" else 1
 
-    dose = predict_using_level.predict_dose(age,int(sex),level)
-    message = "The predicted dose to achieve a level of {} microgram/ml is {} mg".format(level,round(dose))
+    dose = predict.predict_dose(age,int(sex),level)
+    dose_class = predict.is_close(dose)
 
-    return render_template('prediction.html',
+    if comorb == 'yes':
+        approp = dose_class[0]
+    else:
+        approp = dose_class[1]
+
+    if comorb == 'yes':
+        message = "The predicted dose to achieve a level of {} microgram/ml is {} mg. The nearest dose class is {}mg. Since, the patient has comorbidities, the recommended dose is {}mg".format(level, round(dose), dose_class, approp)
+    else:
+        message = "The predicted dose to achieve a level of {} microgram/ml is {} mg. The nearest dose class is {}mg. Since, the patient does not have comorbidities, the recommended dose is {}mg".format(level, round(dose), dose_class, approp)
+
+
+    return render_template('index.html',
                            message = message)
 
 @app.route('/using_dose',methods=['POST'])
@@ -29,11 +47,11 @@ def using_dose():
 
     sex = 0 if sex.casefold() == "female" else 1
 
-    level = predict_using_level.predict_level(age,int(sex),dose)
+    level = predict.predict_level(age,int(sex),dose)
 
     message = "The predicted level using a dose of {} mg is {} microgram/ml".format(dose, level)
 
-    return render_template('prediction.html',
+    return render_template('index.html',
                            message = message)
 
 if __name__ == '__main__':
--- src/predict.py
+++ src/predict.py
@@ -2,6 +2,14 @@ import pickle
 import numpy as np
 
 
+def is_close(x):
+    if x%100 >= 0 and x%100 <=50:
+        ret_class = [x-x%100,x-x%100+50]
+    elif x%100 >=50 and x%100 <=99:
+        ret_class = [x-x%100+50,x-x%100+100]
+
+    return ret_class
+
 def predict_dose(age,sex,level):
     try:
         model = pickle.load(open("models/rf_dose.sav", 'rb'))
